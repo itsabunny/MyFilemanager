95a0d69cb99e1de7197faba1c6d82cb72f9a795c
itsabunny <itsabunny@pm.me>
2025-03-07 14:57:15 +0100

    feat: refactor UserService to use constructor injection for UserRepository

--- backend/filemanager-main/src/main/java/se/kaninis/filemanager/users/UserService.java
+++ backend/filemanager-main/src/main/java/se/kaninis/filemanager/users/UserService.java
@@ -1,6 +1,5 @@
 package se.kaninis.filemanager.users;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -8,10 +7,13 @@ import java.util.Optional;
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    public Optional<UserEntity> findByUsername(String username){
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<UserEntity> findByUsername(String username) {
         return userRepository.findByUsername(username);
     }
 }
