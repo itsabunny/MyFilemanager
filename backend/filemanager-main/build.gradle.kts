2284bf43ebf7f4e3088e9cffb35e055b3de65558
itsabunny <itsabunny@pm.me>
2025-03-08 19:05:49 +0100

    feat: update PostgreSQL dependency version in build.gradle.kts

--- backend/filemanager-main/build.gradle.kts
+++ backend/filemanager-main/build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.security:spring-security-oauth2-jose")
     implementation("org.springframework.security:spring-security-oauth2-resource-server")
-    implementation("org.postgresql:postgresql")
+    implementation("org.postgresql:postgresql:42.5.1")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
