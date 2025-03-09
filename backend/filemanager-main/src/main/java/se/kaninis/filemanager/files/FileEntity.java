e8b6167401e4ad16e32f39cb52f65a5afd4a700f
itsabunny <itsabunny@pm.me>
2025-03-09 17:17:41 +0100

    feat: add comments to FileEntity class for improved code documentation

--- backend/filemanager-main/src/main/java/se/kaninis/filemanager/files/FileEntity.java
+++ backend/filemanager-main/src/main/java/se/kaninis/filemanager/files/FileEntity.java
@@ -7,25 +7,43 @@ import se.kaninis.filemanager.users.UserEntity;
 
 import java.sql.Blob;
 
+/**
+ * Entity-klass som representerar en fil i systemet.
+ */
 @Entity
 @Data
 public class FileEntity {
 
+    /**
+     * Unikt ID för filen.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * Filens namn.
+     */
     private String name;
 
+    /**
+     * Filens innehåll lagrat som en Blob.
+     */
     @Lob
     @Column(columnDefinition = "BYTEA")
     private Blob content;
 
+    /**
+     * Mappen som filen tillhör (kan vara null om den ligger på root-nivå).
+     */
     @ManyToOne
     @JoinColumn(name = "folder_id")
     private FolderEntity folder;
 
+    /**
+     * Användaren som äger filen.
+     */
     @ManyToOne
     @JoinColumn(name = "user_id")
-    private UserEntity owner; // Ny kolumn för att koppla filen till användaren!
+    private UserEntity owner;
 }
