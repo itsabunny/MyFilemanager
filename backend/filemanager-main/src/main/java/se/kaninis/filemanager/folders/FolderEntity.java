23afe709181c665771c5257440fac11718d4b6fb
itsabunny <itsabunny@pm.me>
2025-03-09 17:18:25 +0100

    feat: enhance FolderEntity with additional properties for parent folder and subfolders

--- backend/filemanager-main/src/main/java/se/kaninis/filemanager/folders/FolderEntity.java
+++ backend/filemanager-main/src/main/java/se/kaninis/filemanager/folders/FolderEntity.java
@@ -7,20 +7,48 @@ import se.kaninis.filemanager.users.UserEntity;
 
 import java.util.List;
 
+/**
+ * Entity-klass som representerar en mapp i systemet.
+ */
 @Entity
 @Data
 public class FolderEntity {
 
+    /**
+     * Unikt ID för mappen.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * Mappens namn.
+     */
     private String name;
 
-    @OneToMany(mappedBy = "folder", cascade = CascadeType.ALL)
+    /**
+     * Lista över filer som finns i mappen.
+     */
+    @OneToMany(mappedBy = "folder", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<FileEntity> files;
 
+    /**
+     * Användaren som äger mappen.
+     */
     @ManyToOne
     @JoinColumn(name = "user_id")
     private UserEntity owner;
+
+    /**
+     * Överordnad mapp (om denna mapp ligger i en annan mapp).
+     */
+    @ManyToOne
+    @JoinColumn(name = "parent_folder_id")
+    private FolderEntity parentFolder;
+
+    /**
+     * Lista över undermappar.
+     */
+    @OneToMany(mappedBy = "parentFolder", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<FolderEntity> subFolders;
 }
